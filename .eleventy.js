e615b99f76be199e9b752e74259c6b1a9a68d7ff
James Deane <25thhour+git@gmail.com>
2021-12-23 11:48:09 +1300

    update src config paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_redirects");
+
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
