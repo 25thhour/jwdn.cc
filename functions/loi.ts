cb1ff517a93c5d5e70805dbc64d8be4d2538c7fd
James Deane <25thhour+git@gmail.com>
2022-01-12 12:22:29 +1300

    update loi to accept a city param and cache the response

--- functions/loi.ts
+++ functions/loi.ts
@@ -1,17 +1,53 @@
-export async function onRequest(ctx) {
-  const init = {
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
-  }
-  const response = await fetch('https://locations.covid19.health.nz/api/loi?search=&sort=updated&order=DESC&city=Christchurch&suburb=All', init)
-  const data = await response.json()
-  const { locations } = data
-  console.log(locations)
+// const validCities = ['Auckland', 'Bay of Islands', 'Bulls', 'Carterton', 'Christchurch', 'Feilding', 'Hamilton', 'Lower Hutt', 'Manawatu-Wanganui', 'Masterton', 'Mount Maunganui', 'New Plymouth', 'Opotiki', 'Orewa', 'Otaki', 'Otorohanga', 'Palmerston North', 'Papamoa', 'Queenstown', 'Rotorua', 'Takanini', 'Taupō', 'Tauranga', 'Waihi', 'Waihi Beach', 'Waimauku', 'Warkworth', 'Wellington']
 
-  return new Response(JSON.stringify(locations, null, 2), {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-  });
+async function getLocationData(city?: string) {
+  const location = city || 'Christchurch'
+  const url = `https://locations.covid19.health.nz/api/loi?search=&sort=updated&order=DESC&city=${location}&suburb=All`
+  const response = await fetch(url, {
+  headers: {
+    'content-type': 'application/json'
+  },
+  referrerPolicy: 'no-referrer'
+})
+
+return response.json()
 }
+
+export async function onRequest(ctx) {
+  const event = ctx.event
+  const request = ctx.request
+  const cacheUrl = new URL(request.url)
+  const city = cacheUrl.searchParams.get('city') || ''
+  console.log(city)
+
+  // Construct the cache key from the cache URL
+  const cacheKey = new Request(cacheUrl.toString(), request)
+  const cache = caches.default
+
+  // Check whether the value is already available in the cache
+  // if not, you will need to fetch it from origin, and store it in the cache
+  // for future access
+  let response = await cache.match(cacheKey)
+
+  if (!response) {
+    // If not in cache, get it from origin
+    const { locations } = await getLocationData(city)
+
+    console.log(locations)
+    // Must use Response constructor to inherit all of response's fields
+    response = new Response(JSON.stringify(locations, null, 2), response)
+
+    // Cache API respects Cache-Control headers. Setting s-max-age to 1800
+    // will limit the response to be in cache for 1800 seconds max
+
+    // Any changes made to the response here will be reflected in the cached value
+    response.headers.append("Cache-Control", "s-maxage=1800")
+
+    // Store the fetched response as cacheKey
+    // Use waitUntil so you can return the response without blocking on
+    // writing to cache
+    ctx.waitUntil(cache.put(cacheKey, response.clone()))
+  }
+  return response
+}
+
